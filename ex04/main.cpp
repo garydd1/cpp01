ad7258f803e904845834be91f7d75b8da6498414
garydd1 <dgarizado@gmail.com>
2023-07-14 17:07:45 +0200

    updates

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,9 +6,12 @@
 // Instead, it will replace all occurrences of the first string with the second string.
 int main (int argc, char *argv[]) {
 	
+	std::string line;
+
 	if (argc != 4)
 	{
 		std::cout << "Wrong number of arguments\n";
+		return 1; 
 	}
 
 	std::ifstream file(argv[1]);
@@ -17,21 +20,22 @@ int main (int argc, char *argv[]) {
 		std::cout << "Error opening file\n";
 		return 1;
 	}
-	std::string line;
 	std::ofstream newFile(argv[1] + std::string(".replace"));
 	if (!newFile)
 	{
 		std::cout << "Error opening file\n";
 		return 1;
 	}
-
+	size_t pos = 0;
 	while (std::getline(file, line))
 	{
-		size_t pos = 0;
-		while ((pos = line.find(argv[2], pos)) != std::string::npos)
+		pos = line.find(argv[2]);
+		while (pos != std::string::npos)
 		{
-			line.replace(pos, std::string(argv[2]).length(), argv[3]);
-			pos += std::string(argv[3]).length();
+			line.erase(pos, std::string(argv[2]).length());
+			line.insert(pos, argv[3], std::string(argv[3]).length());
+			pos += std::string(argv[3]).length() - std::string(argv[2]).length();
+			pos = line.find(argv[2], pos);
 		}
 		newFile << line << std::endl;
 	}
